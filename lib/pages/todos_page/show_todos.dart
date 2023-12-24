18db799de73fca46a0723f01497a57ee5e789d36
yhan0704 <hanyc1205@gmail.com>
2023-12-24 13:05:08 -0500

    Todoitem widget

--- lib/pages/todos_page/show_todos.dart
+++ lib/pages/todos_page/show_todos.dart
@@ -1,6 +1,9 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+
 import 'package:todo_cubit/cubits/cubits.dart';
+import 'package:todo_cubit/models/todo_model.dart';
 
 class ShowTodo extends StatelessWidget {
   const ShowTodo({super.key});
@@ -15,11 +18,35 @@ class ShowTodo extends StatelessWidget {
       itemBuilder: (BuildContext context, int index) {
         return Dismissible(
           key: ValueKey(todos[index].id),
-          child: Text(
-            todos[index].desc,
-            style: TextStyle(fontSize: 20.0),
+          child: TodoItem(
+            todo: todos[index],
           ),
           background: showBackground(0),
+          onDismissed: (_) {
+            context.read<TodoListCubit>().removeTodo(todos[index]);
+          },
+          confirmDismiss: (_) {
+            return showDialog(
+              context: context,
+              barrierDismissible: false,
+              builder: (context) {
+                return AlertDialog(
+                  title: Text('Are you sure?'),
+                  content: Text('Do you really want to delete?'),
+                  actions: [
+                    TextButton(
+                      onPressed: () => Navigator.pop(context, false),
+                      child: Text('NO'),
+                    ),
+                    TextButton(
+                      onPressed: () => Navigator.pop(context, true),
+                      child: Text('YES'),
+                    ),
+                  ],
+                );
+              },
+            );
+          },
           secondaryBackground: showBackground(1),
         );
       },
@@ -43,6 +70,88 @@ class ShowTodo extends StatelessWidget {
         size: 30.0,
         color: Colors.white,
       ),
+    );
+  }
+}
+
+class TodoItem extends StatefulWidget {
+  final Todo todo;
+  const TodoItem({
+    Key? key,
+    required this.todo,
+  }) : super(key: key);
+
+  @override
+  State<TodoItem> createState() => _TodoItemState();
+}
+
+class _TodoItemState extends State<TodoItem> {
+  late final TextEditingController textController;
+
+  @override
+  void initState() {
+    super.initState();
+    textController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    textController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      onTap: () {
+        showDialog(
+            context: context,
+            builder: (context) {
+              bool _error = false;
+              textController.text = widget.todo.desc;
+
+              return StatefulBuilder(
+                builder: (BuildContext context, StateSetter setState) {
+                  return AlertDialog(
+                    title: Text("edit todo"),
+                    content: TextField(
+                      controller: textController,
+                      autofocus: true,
+                      decoration: InputDecoration(
+                        errorText: _error ? "Vlaue cannot be empty" : null,
+                      ),
+                    ),
+                    actions: [
+                      TextButton(
+                        onPressed: () => Navigator.pop(context),
+                        child: Text("Cancel"),
+                      ),
+                      TextButton(
+                        onPressed: () => {
+                          setState(() {
+                            _error = textController.text.isEmpty ? true : false;
+                            if (!_error) {
+                              context.read<TodoListCubit>().editTodo(
+                                  widget.todo.id, textController.text);
+                              Navigator.pop(context);
+                            }
+                          })
+                        },
+                        child: Text("Edit"),
+                      )
+                    ],
+                  );
+                },
+              );
+            });
+      },
+      leading: Checkbox(
+        value: widget.todo.completed,
+        onChanged: (bool? checked) {
+          context.read<TodoListCubit>().toggleTodo(widget.todo.id);
+        },
+      ),
+      title: Text(widget.todo.desc),
     );
   }
 }
